83bb6143abdc2d0755147d517a37a967cf343c96
Rikke Sommer Haaber <rsh@clin.au.dk>
2025-12-03 10:04:52 +0100

    Made meanSD into a function

---
+++ R/functions.R
@@ -0,0 +1,18 @@
+
+#' Table 1 mean SD
+#'
+#' @param data
+#' @param col
+#'
+#' @returns table 1
+#' @export
+#'
+#' @examples
+create_table_descriptive_stats <- function(data, col) {
+  data |>
+    dplyr::group_by({{ col }}) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::mutate(dplyr::across(dplyr::where(is.numeric), \(x) base::round(x, digits = 1))) |> # runder alle numeriske værdier til 1 decimal
+    mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |> # sætter de to sammen i én værdi i tabellen og parenteserne er for at få sd i en parentes
+    dplyr::select(Metabolite = {{ col }}, "Mean SD" = MeanSD) # "nyt navn = col i data", man vælger i select det, der skal være output i tabellen
+}
